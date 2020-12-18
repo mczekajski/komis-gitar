53288495474892d2258b517e7cad2383ffdf20f2
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-18 11:20:13 +0100

    Add first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GuitarCardComponent } from './guitar-card/guitar-card.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuitarCardComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/guitar-card/guitar-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guitar-card',
+  templateUrl: './guitar-card.component.html',
+  styleUrls: ['./guitar-card.component.scss']
+})
+export class GuitarCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
