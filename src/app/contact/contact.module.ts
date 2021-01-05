8b8876b9b2976f627968604cf6e541b4d2964b41
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-05 19:29:32 +0100

    Add contact elements

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactHomeComponent } from './contact-home/contact-home.component';
 import { ContactRoutingModule } from './contact-routing.module';
+import { MapComponent } from './map/map.component';
 
 
 
 @NgModule({
-  declarations: [ContactHomeComponent],
+  declarations: [ContactHomeComponent, MapComponent],
   imports: [
     CommonModule,
     ContactRoutingModule
